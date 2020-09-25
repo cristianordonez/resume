b2e14c4266aeee62518ecd904aa0102271107a8e
cristianordonez94 <62552837+cristianordonez94@users.noreply.github.com>
2020-09-25 12:56:32 -0400

    added the recaptcha on contact form

--- src/components/contactus/contactus.js
+++ src/components/contactus/contactus.js
@@ -25,25 +25,42 @@ export default class ContactUs extends  Component {
         <div className="row">
           <div className="eight columns">
         
+
+
             {/* form */}  
-            <form action method="post" id="contactForm" name="contactForm">
+            <form action method="POST" id="contactForm" name="contactForm" data-netlify="true">
               <fieldset>
+
                 <div>
                   <label htmlFor="contactName">Name <span className="required">*</span></label>
                   <input type="text" defaultValue size={35} id="contactName" name="contactName" />
                 </div>
+
+
                 <div>
                   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
                   <input type="text" defaultValue size={35} id="contactEmail" name="contactEmail" />
                 </div>
+
+
                 <div>
                   <label htmlFor="contactSubject">Subject</label>
                   <input type="text" defaultValue size={35} id="contactSubject" name="contactSubject" />
                 </div>
+
+
                 <div>
                   <label htmlFor="contactMessage">Message <span className="required">*</span></label>
                   <textarea cols={50} rows={15} id="contactMessage" name="contactMessage" defaultValue={""} />
                 </div>
+
+
+                <div data-netlify-recaptcha="true">
+                </div>
+                
+
+
+
                 <div>
                   <button className="submit">Submit</button>
                   <span id="image-loader">
