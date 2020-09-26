afea5d4bbc73d6bfc4d69782fc9d087583c39576
cristianordonez <cristianordonezrd@gmail.com>
2020-09-26 16:48:46 -0400

    added about section

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -12,12 +12,9 @@ export default class About extends Component {
             </div>
             <div className="nine columns main-col">
               <h2>About Me</h2>
-              <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-                voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-                voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-                sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.
+              <p>My love for nutrition comes from my love for exercise and a desire to help others. I have realized that nutrition
+                plays an important part in every aspect of our life and can be used to improve our overall well-being. I also have an interest in learning new skills 
+                and hobbies like graphic design or coding, which I hope is showcased throughout this portfolio!
               </p>
               <div className="row">
                 <div className="columns contact-details">
