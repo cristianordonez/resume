eb96fc51b030cf5dd837a2c1cd36b8091042e875
cristianordonez <cristianordonezrd@gmail.com>
2020-09-26 16:54:55 -0400

    added my resume to download buton in about section

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -25,12 +25,12 @@ export default class About extends Component {
                       Morganville, NJ 07751 US
                     </span><br />
                     <span>(732)912-4457</span><br />
-                    <span>cristianordonezRD.com</span>
+                    <span>cristianordonezRD@gmail.com</span>
                   </p>
                 </div>
                 <div className="columns download">
                   <p>
-                    <a href="#" className="button"><i className="fa fa-download" />Download Resume</a>
+                    <a href="images/Resume2020.pdf" className="button"><i className="fa fa-download" />Download Resume</a>
                   </p>
                 </div>
               </div> {/* end row */}
