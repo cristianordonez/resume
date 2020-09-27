26ce26dede05ae671a0c7535beb5aaab5d745d32
cristianordonez <cristianordonezrd@gmail.com>
2020-09-27 16:27:56 -0400

    finished the skills portion of resume.js

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -113,12 +113,12 @@ export default class Resume extends Component {
               </div> {/* item end */}
 
 
-
-
-
-
             </div> {/* main-col end */}
           </div> {/* End Work */}
+
+
+
+
           {/* Skills
         ----------------------------------------------- */}
           <div className="row skill">
@@ -126,19 +126,19 @@ export default class Resume extends Component {
               <h1><span>Skills</span></h1>
             </div>
             <div className="nine columns main-col">
-              <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-                voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-                voluptatem sequi nesciunt.
+              <p>Through my experience and education I have been able to gain many skills in a variety of different areas. 
+                These include technical, interpersonal and analytical skills that will allow me excel as a Registered Dietitian. Some 
+                skills I have gained include the following:
               </p>
               <div className="bars">
                 <ul className="skills">
-                  <li><span className="bar-expand photoshop" /><em>Photoshop</em></li>
-                  <li><span className="bar-expand illustrator" /><em>Illustrator</em></li>
-                  <li><span className="bar-expand wordpress" /><em>Wordpress</em></li>
-                  <li><span className="bar-expand css" /><em>CSS</em></li>
-                  <li><span className="bar-expand html5" /><em>HTML5</em></li>
-                  <li><span className="bar-expand jquery" /><em>jQuery</em></li>
+                  <li><span className="bar-expand photoshop" /><em>Adaptability</em></li>
+                  <li><span className="bar-expand illustrator" /><em>Fluency in spanish</em></li>
+                  <li><span className="bar-expand wordpress" /><em>Written Communication</em></li>
+                  <li><span className="bar-expand css" /><em>CSS, HTML5 and Javascript</em></li>
+                  <li><span className="bar-expand html5" /><em>Analytical Skills</em></li>
+                  <li><span className="bar-expand jquery" /><em>React and Node.js</em></li>
+                  <li><span className="bar-expand css" /><em>Affinity Designer, Photo and Illustrator</em></li>
                 </ul>
               </div>{/* end skill-bars */}
             </div> {/* main-col end */}
