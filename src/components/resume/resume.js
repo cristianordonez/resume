9189796210db2a0b0cea53b5e06064f0fc7bdf8e
cristianordonez <cristianordonezrd@gmail.com>
2020-09-27 16:11:04 -0400

    finished adding the work or experience section

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -49,30 +49,74 @@ export default class Resume extends Component {
               <h1><span>Experience</span></h1>
             </div>
             <div className="nine columns main-col">
+
+
+
+
               <div className="row item">
                 <div className="twelve columns">
-                  <h3>Awesome Design Studio</h3>
-                  <p className="info">Senior UX Designer <span>•</span> <em className="date">March 2010 - Present</em></p>
+                  <h3>Louis Stokes Cleveland Veteran Affairs Medical Center Dietetic Internship</h3>
+                  <p className="info">Dietetic Intern <span>•</span> <em className="date">August 2019 - July 2020</em></p>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
-                    Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-                    ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                    Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. Nullam dictum felis eu pede mollis pretium.
+                  Completed 1200 hours of supervised practice in Medical Nutrition Therapy, Foodservice 
+                  Systems Management and Community Nutrition in accordance with practice areas of Registered Dietitians.
+                  Developed and provided extensive nutrition education in areas including Diabetes Self-management, Renal, 
+                  Spinal Cord Injury, Oncology, Cardiovascular Risk Reduction, General Medicine and Weight Management. 
+                  Facilitated over 25 hours of the VA’s MOVE! Weight Management program in groups 
+                  of between 5 and 20 Veterans, completed both in a classroom setting as well as through VA Video Connect.
                   </p>
                 </div>
               </div> {/* item end */}
+
+
+
+
+
               <div className="row item">
                 <div className="twelve columns">
-                  <h3>Super Cool Studio</h3>
-                  <p className="info">UX Designer <span>•</span> <em className="date">March 2007 - February 2010</em></p>
+                  <h3>Cooking Matters for Kids</h3>
+                  <p className="info">Nutrition Instructor <span>•</span> <em className="date">February 2019 - April 2019</em></p>
                   <p>
-                    This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                    Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem
-                    nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan
-                    ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat
+                  Discussed strategies on how to incorporate nutritionally dense foods into daily diet with 
+                  over 15 elementary aged students to improve meal selection for low income families.
+                  Instruct tailored weekly lessons that create an intellectually engaging atmosphere for elementary students. 
                   </p>
                 </div>
               </div> {/* item end */}
+
+
+              <div className="row item">
+                <div className="twelve columns">
+                  <h3>Eastern Michigan University</h3>
+                  <p className="info">Sports Nutrition Intern <span>•</span> <em className="date">May 2018 - August 2018</em></p>
+                  <p>
+                  Analyzed food intakes for over 600 student athletes across 17 sports to assure adequate caloric intake.
+                Developed a presentation regarding how student athletes can improve overall food intake to improve 
+                nutritional status.
+                  </p>
+                </div>
+              </div> {/* item end */}
+
+
+
+              <div className="row item">
+                <div className="twelve columns">
+                  <h3>Ypsilanti Meals on Wheels</h3>
+                  <p className="info">Volunteer <span>•</span> <em className="date">August 2017 - December 2017</em></p>
+                  <p>
+                  Counseled 10 elderly participants weekly to provide companionship to isolated individuals
+                   improving food access and their mental and physical well-being.
+                  Maximized efficiency through creating improved volunteer driver routes to decrease transit time. 
+ 
+                  </p>
+                </div>
+              </div> {/* item end */}
+
+
+
+
+
+
             </div> {/* main-col end */}
           </div> {/* End Work */}
           {/* Skills
